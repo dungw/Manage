ba917bb08f631e530342f55314472de83949a945
Dzung Vuong <jobhelper2008@gmail.com>
2015-08-18 03:18:02 +0700

    update

---
+++ common/models/LoginForm.php
@@ -0,0 +1,94 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use frontend\models\SignupForm;
+
+/**
+ * Login form
+ */
+class LoginForm extends Model
+{
+    public $username;
+    public $password;
+    public $rememberMe = true;
+
+    private $_user = false;
+
+    // duration login if remember
+    private $rememberDuration = 86400;
+
+    private $timeout = 86400;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            // username and password are both required
+            [['username', 'password'], 'required'],
+            // rememberMe must be a boolean value
+            ['rememberMe', 'boolean'],
+            // password is validated by validatePassword()
+            ['password', 'validatePassword'],
+        ];
+    }
+
+    /**
+     * Validates the password.
+     * This method serves as the inline validation for password.
+     *
+     * @param string $attribute the attribute currently being validated
+     * @param array $params the additional name-value pairs given in the rule
+     */
+    public function validatePassword($attribute, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, 'Tên đăng nhập hoặc mật khẩu không đúng.');
+            }
+        }
+    }
+
+    /**
+     * Logs in a user using the provided username and password.
+     *
+     * @return boolean whether the user is logged in successfully
+     */
+    public function login()
+    {
+        if ($this->validate()) {
+            $duration = $this->timeout;
+
+            $user = Yii::$app->user->login($this->getUser(), $duration);
+
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Finds user by [[username]]
+     *
+     * @return User|null
+     */
+    public function getUser()
+    {
+        if ($this->_user === false) {
+            $this->_user = User::findByUsername($this->username);
+        }
+
+        return $this->_user;
+    }
+
+    public function attributeLabels() {
+        return [
+            'username' => 'Tên đăng nhập',
+            'password' => 'Mật khẩu',
+        ];
+    }
+}
